a2dab099b75390ce8d2bb6b8dc698239f9f889d0
fillooow <fil201121@yandex.ru>
2020-09-03 13:02:19 +0500

    задаем краску, задаем общую отрисовку

--- app/src/main/java/fillooow/app/minipaint/CustomCanvasView.kt
+++ app/src/main/java/fillooow/app/minipaint/CustomCanvasView.kt
@@ -3,13 +3,29 @@ package fillooow.app.minipaint
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Paint
 import android.view.View
 import androidx.core.content.res.ResourcesCompat
+
+private const val STROKE_WIDTH = 12f
 
 class CustomCanvasView(context: Context) : View(context) {
 
     private lateinit var extraCanvas: Canvas
     private lateinit var extraBitmap: Bitmap
+
+    private val drawColor = ResourcesCompat.getColor(resources, R.color.colorPaint, null)
+
+    private val paint = Paint().apply {
+
+        color = drawColor
+        isAntiAlias = true // smooth out edges
+        isDither = true // понижает количество цветов до 256 (вроде). Короч, понижение размерности
+        style = Paint.Style.STROKE
+        strokeJoin = Paint.Join.ROUND
+        strokeCap = Paint.Cap.ROUND
+        strokeWidth = STROKE_WIDTH
+    }
 
     private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
 
@@ -22,5 +38,10 @@ class CustomCanvasView(context: Context) : View(context) {
         extraCanvas = Canvas(extraBitmap)
 
         extraCanvas.drawColor(backgroundColor)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(extraBitmap, 0f, 0f, null)
     }
 }
