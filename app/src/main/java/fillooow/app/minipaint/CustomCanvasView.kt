0194ce9e81e207ca2bcf05741125cedce6e64fc5
fillooow <fil201121@yandex.ru>
2020-09-03 12:32:38 +0500

    обработал реагирование вьюхи на изменение размера

--- app/src/main/java/fillooow/app/minipaint/CustomCanvasView.kt
+++ app/src/main/java/fillooow/app/minipaint/CustomCanvasView.kt
@@ -1,9 +1,26 @@
 package fillooow.app.minipaint
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.view.View
+import androidx.core.content.res.ResourcesCompat
 
 class CustomCanvasView(context: Context) : View(context) {
 
+    private lateinit var extraCanvas: Canvas
+    private lateinit var extraBitmap: Bitmap
 
+    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        if (::extraBitmap.isInitialized) extraBitmap.recycle()
+
+        extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        extraCanvas = Canvas(extraBitmap)
+
+        extraCanvas.drawColor(backgroundColor)
+    }
 }
